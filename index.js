0864b8b7f84b66d02f2070c8bfa77133bd74e878
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-03 19:59:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,18 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  return "I can't hear you!" if `string` 
+  var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
   
 }
