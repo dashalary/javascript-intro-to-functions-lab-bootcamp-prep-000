818665cad3aa290e24aa62e259352f6175b6a60c
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-03 22:01:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ function sayHiToGrandma(string) {
   } if (string.toUpperCase()===string) {
     return "YES INDEED!";
   } if (string==='I love you, Grandma.' {
-    
+    return "I love you, too."
+  }
   }
   }
   }
