94ed4a737e2ef9e79abaa139f7f97226b5cf34a1
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-03 19:46:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ function whisper(string) {
   return
   string.toLowerCase()
 }
+
 function logShout(string) {
   console.log
   string.toUpperCase()
 }
+
+function logWhisper(string) {
+  console.log
+  string.toLowerCase()
+}
+
