4685deee7b960b16176f2f331b3ce667d074617b
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-03 20:48:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,14 +19,23 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+  var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
   console.log("I can't hear you!")
 return `${I love you}, ${Grandma}!`
 console.log('YES INDEED!')
 return `${I love you}, ${Grandma}!`
 console.log('I love you, too.')
 return `${I love you}, ${Grandma}!`
- 
-
-
-  
 }
