88955d3953341ef37f83c143c5c677cb50c29c5f
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-03 22:13:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var lowercase, uppercase;
 
-function shout(string) {
+function shout(string){
   return
   string.toUpperCase();
 }
 
-function whisper(string) {
+function whisper(string){
   return
   string.toLowerCase();
 }
